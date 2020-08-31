6e24a0801297fb81bdbc7e2f435d048720c0d839
Luciano Pierdona <lucianompj1@gmail.com>
2020-08-31 17:58:17 -0300

    finished first version

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import Modal from 'react-modal';
+import Layout from '../components/Layout';
+import Rates from '../components/rates';
+
+Modal.setAppElement("#__next");
+
+const currencies = ["CAD", "USD", "GBP", "MXN", "AUD", "EUR", "NOK", "BRL"];
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <ul>
+        {currencies.map((currency) => ( 
+            <li key={currency} className="huge">
+              <Link 
+                href={`/?currencyCode=${currency}`} 
+                as={`/currency/${currency}`}
+              > 
+                <a>{currency}</a>
+              </Link>
+            </li> 
+        ))}
+      </ul>
+      <Modal isOpen={!!router.query.currencyCode} onRequestClose={() => router.push("/")}>
+        <Rates currencyCode={router.query.currencyCode} />
+      </Modal>
+    </Layout>
+  );
+}
